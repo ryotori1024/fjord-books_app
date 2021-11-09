575b86852d3ca480def94524fac436e7674caa0f
ryotori <ko_re_zo_n@yahoo.co.jp>
2021-11-09 23:59:46 +0900

    Active Storage課題 提出物作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_one_attached :image
+
+  validate :image_content_type
+
+  private
+
+  def image_content_type
+    extension = ['image/png', 'image/jpeg', 'image/gif']
+    errors.add(:image, 'の拡張子はpng、jpg、jpeg、gifのみを指定してください') unless image.content_type.in?(extension)
+  end
 end
